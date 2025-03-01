e52609ccf4332ba110b008fb55a6347742af21e8
Manny Cortes <polosetroc@gmail.com>
2025-02-28 23:41:32 -0800

    fix: einops issue

--- time_series/wcmha.py
+++ time_series/wcmha.py
@@ -24,34 +24,36 @@ def butterworth_filter(scale, order, times):
 
 
 # 3.1 Weighted Causal Multihead Attention
-class WeightedCausalMultiheadAttention(nn.Module):
+class WeightedCausalMultiheadAttention(torch.nn.Module):
     def __init__(
         self,
-        embed_dim,
+        d_model,
         num_heads,
-        f_type: Literal["spl", "wpl", "butterworth"] = "spl",
+        locality_func: Literal[
+            "similarity_power_law", "weighted_power_law", "butterworth_filter"
+        ] = ("similarity_power_law"),
         alpha=1.0,
         butterworth_tc=None,
     ):
         super().__init__()
-        self.embed_dim = embed_dim
+        self.embed_dim = d_model
         self.num_heads = num_heads
-        self.head_dim = embed_dim // num_heads
-        assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads"
+        self.head_dim = d_model // num_heads
+        assert d_model % num_heads == 0, "embed_dim must be divisible by num_heads"
 
-        self.W_q = nn.Linear(embed_dim, embed_dim)
-        self.W_k = nn.Linear(embed_dim, embed_dim)
-        self.W_v = nn.Linear(embed_dim, embed_dim)
-        self.W_o = nn.Linear(embed_dim, embed_dim)
+        self.W_q = torch.nn.Linear(d_model, d_model)
+        self.W_k = torch.nn.Linear(d_model, d_model)
+        self.W_v = torch.nn.Linear(d_model, d_model)
+        self.W_o = torch.nn.Linear(d_model, d_model)
 
-        self.f_type = f_type
+        self.f_type = locality_func
         self.alpha = alpha
-        if f_type == "butterworth":
+        if locality_func == "butterworth_filter":
             assert butterworth_tc is not None, "Butterworth filter requires cutoff time"
             self.butterworth_tc = butterworth_tc
 
     def forward(self, x):
-        batch, T, D = x.shape
+        _, T, _ = x.shape
         Q = self.W_q(x)
         K = self.W_k(x)
         V = self.W_v(x)
@@ -60,7 +62,7 @@ class WeightedCausalMultiheadAttention(nn.Module):
         V = rearrange(V, "b t (h d) -> b h t d", h=self.num_heads)
 
         # S_h
-        scores = torch.einsum("b h t_i d, b h t_j d -> b h t_i t_j", Q, K) / (
+        scores = torch.einsum("b h i d, b h j d -> b h i j", Q, K) / (
             self.head_dim**0.5
         )
 
@@ -71,12 +73,12 @@ class WeightedCausalMultiheadAttention(nn.Module):
 
         time_idx = torch.arange(T, device=x.device)
         delta = time_idx.unsqueeze(1) - time_idx.unsqueeze(0)  # delta[i,j] = i - j
-        if self.f_type == "spl":
+        if self.f_type == "similarity_power_law":
             decaying_mask = -(delta.float() ** self.alpha)
             decaying_mask = torch.where(
                 delta < 0, torch.zeros_like(decaying_mask), decaying_mask
             )
-        elif self.f_type == "wpl":
+        elif self.f_type == "weighted_power_law":
             safe_delta = torch.where(
                 delta > 0, delta.float(), torch.ones_like(delta.float())
             )
@@ -84,14 +86,14 @@ class WeightedCausalMultiheadAttention(nn.Module):
             decaying_mask = torch.where(
                 delta <= 0, torch.zeros_like(decaying_mask), decaying_mask
             )
-        elif self.f_type == "butterworth":
+        elif self.f_type == "butterworth_filter":
             positive_delta = delta.float().clamp(min=0).cpu().numpy()
             butter_mask = butterworth_filter(
                 self.butterworth_tc, int(self.alpha), positive_delta
             )
             decaying_mask = torch.tensor(butter_mask, device=x.device, dtype=x.dtype)
         else:
-            raise ValueError("Unknown f_type")
+            raise ValueError("Unknown locality function")
 
         scores = scores + decaying_mask.unsqueeze(0).unsqueeze(
             0
